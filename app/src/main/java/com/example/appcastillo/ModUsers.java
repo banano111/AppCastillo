5242c47a204761303a03c49baee1819b9e1ace68
Alan Pedraza <alanbanano@gmail.com>
2019-11-25 06:10:00 -0600

    La aplicacion ya actualiza datos y los elimina

--- app/src/main/java/com/example/appcastillo/ModUsers.java
+++ app/src/main/java/com/example/appcastillo/ModUsers.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import com.example.appcastillo.modelos.Usuario;
 import com.google.firebase.FirebaseApp;
@@ -19,14 +24,18 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class ModUsers extends AppCompatActivity {
 
     ListView usuariosBD;
-    Button btnCasa;
+    Button btnCasa, btnSave, btnDelete;
+    EditText nombre, apellidoP,apellidoM,deporteF;
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+
+    Usuario usuarioSelected;
 
     private List<Usuario> listPerson = new ArrayList<>();
     ArrayAdapter<Usuario> arrayAdapterPersona;
@@ -37,11 +46,110 @@ public class ModUsers extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_mod_users);
 
+            nombre = findViewById(R.id.nombre);
+            apellidoP = findViewById(R.id.apellidoP);
+            apellidoM = findViewById(R.id.apellidoM);
+            deporteF = findViewById(R.id.deporteF);
+
             usuariosBD = findViewById(R.id.vistaUsers);
 
             inicializarFirebase();
             listarDatos();
-            
+
+            usuariosBD.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    usuarioSelected = (Usuario) parent.getItemAtPosition(position);
+                    nombre.setText(usuarioSelected.getNombreUser());
+                    apellidoP.setText(usuarioSelected.getApellidoPUser());
+                    apellidoM.setText(usuarioSelected.getApellidoMUser());
+                    deporteF.setText(usuarioSelected.getDeporteUser());
+                }
+            });
+
+
+            btnSave = findViewById(R.id.botonSave);
+
+            btnSave.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    String name = nombre.getText().toString();
+                    String apeP = apellidoP.getText().toString();
+                    String apeM = apellidoM.getText().toString();
+                    String dep = deporteF.getText().toString();
+
+                    if (name.equals("") || apeP.equals("") || apeM.equals("") || dep.equals("")) {
+                        validacion();
+                    } else {
+                        Usuario user = new Usuario();
+
+                        user.setUid(usuarioSelected.getUid());
+                        user.setNombreUser(nombre.getText().toString().trim());
+                        user.setApellidoPUser(apellidoP.getText().toString().trim());
+                        user.setApellidoMUser(apellidoM.getText().toString().trim());
+                        user.setDeporteUser(deporteF.getText().toString().trim());
+
+                        databaseReference.child("Usuario").child(user.getUid()).setValue(user);
+
+                        Toast.makeText(getApplicationContext(), "Usuario Actualizado, Gracias", Toast.LENGTH_SHORT).show();
+                        limpiaDatos();
+                    }
+
+                }
+
+                    private void limpiaDatos() {
+
+                        nombre.setText("");
+                        apellidoP.setText("");
+                        apellidoM.setText("");
+                        deporteF.setText("");
+                    }
+
+                    private void validacion() {
+
+                        String nameVal = nombre.getText().toString();
+                        String apePVal = apellidoP.getText().toString();
+                        String apeMVal = apellidoM.getText().toString();
+                        String depVal = deporteF.getText().toString();
+
+                        if (nameVal.equals("")) {
+                            nombre.setError("Ingrese Nombre");
+                        } else if (apePVal.equals("")) {
+                            apellidoP.setError("Ingrese Apellido Paterno");
+                        } else if (apeMVal.equals("")) {
+                            apellidoM.setError("Ingrese Apellido Materno");
+                        } else if (depVal.equals("")) {
+                            deporteF.setError("Ingrese Deporte Favorito");
+                        }
+                    }
+
+                });
+
+            btnDelete = findViewById(R.id.botonDelete);
+
+            btnDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Usuario user = new Usuario();
+                    user.setUid(usuarioSelected.getUid());
+                    databaseReference.child("Usuario").child(user.getUid()).removeValue();
+                    Toast.makeText(getApplicationContext(),"Usuario Eliminado", Toast.LENGTH_SHORT).show();
+                    limpiaDatos();
+
+                }
+
+                private void limpiaDatos() {
+
+                    nombre.setText("");
+                    apellidoP.setText("");
+                    apellidoM.setText("");
+                    deporteF.setText("");
+                }
+            });
+
+
             btnCasa = findViewById(R.id.botonCasa);
 
             btnCasa.setOnClickListener(new View.OnClickListener() {
@@ -52,14 +160,16 @@ public class ModUsers extends AppCompatActivity {
 
                     startActivity(casa);
 
+
                 }
             });
+
+
     }
 
     private void inicializarFirebase() {
         FirebaseApp.initializeApp(this);
         firebaseDatabase = FirebaseDatabase.getInstance();
-        //firebaseDatabase.setPersistenceEnabled(true);
         databaseReference = firebaseDatabase.getReference();
     }
 
